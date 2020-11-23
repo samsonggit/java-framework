5d2043bc24db4d46f5a403182aecd610a83b1282
samsonggit <67452315+samsonggit@users.noreply.github.com>
2020-11-23 23:54:28 +0800

    change

---
+++ java-framework/src/sam/java/io/TeztInputsTream.java
@@ -0,0 +1,10 @@
+package sam.java.io;
+
+public class TeztInputsTream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java-framework/src/sam/java/util/TestHashmap.java
@@ -0,0 +1,10 @@
+package sam.java.util;
+
+public class TestHashmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
